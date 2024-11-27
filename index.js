eb4847df2dfa0ca20ea6aad77661862ecbf78193
adva <adva.apelboim@gmail.com>
2024-11-27 17:12:45 +0200

    with-time-module

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const http = require("http");
+const { getDateTime } = require("./time")
+
 const port = 8080;
 
 http.createServer((req, res) => {
+    console.log(getDateTime());
     res.writeHead(200);
     res.write("adva apel");
+    res.write(getDateTime());
     res.end();
 }).listen(port);
 
